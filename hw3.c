0e4ece8960acb81a4b8b8114040991899c1c79ad
Dove411302246 <u11302246@o365.ttu.edu.tw>
2025-05-12 20:20:14 +0800

    finish question 3

--- hw3.c
+++ hw3.c
@@ -2,8 +2,54 @@
 #include<conio.h>
 #include<stdlib.h>
 
+#define ROWS 9
+#define COLS 9
+char seats[ROWS][COLS];
+ 
+void seats_set()//seats setting
+{
+	int i,j,r,c,count,rand_seat=0;
+	for(i=0;i<ROWS;i++)
+	{ 
+        for(j=0;j<COLS;j++)
+        {
+            seats[i][j] = '-';
+        }
+    }
+    while(count<10)//座位小於10時 
+    {
+    	r=rand()%ROWS;//r為隨機數/9 
+    	c=rand()%COLS;//c為隨機數/9 
+    	if (seats[r][c]!='*')
+    	{
+    		seats[r][c]='*';//填入*
+			count++; 
+		}
+	}
+}
 
-
+void show_seats()//show seats
+{
+	int i,j;
+	printf("  ");
+    for (i=0;i<COLS;i++)
+    {
+    	printf("%2d",i+ 1);
+	}
+    printf("\n");
+    
+	for (i=0;i< ROWS;++i)
+	{
+		printf("%2d",i+1);
+        for (j=0;j<COLS;++j)
+        {
+            printf("%2c",seats[i][j]);
+        }
+        printf("\n");
+	}
+	
+}
+ 
 int main()
 {
 	
@@ -31,11 +77,11 @@ int main()
 	printf("--------------------------\n");
 	getch();//偵測按鍵使用 
 	system("CLS");//清除畫面 
-	//輸入密碼
-	int password,i=0,j,k,math;
-	char ch,chwork;
+	//輸入密碼 
+	int password,j=0;
+	char ch;
 	printf("請輸入4位密碼\n");
-	while(i!=3)
+	while(j!=3)
 	{
 		scanf("%d",&password);
 		if(password==2024)
@@ -46,24 +92,34 @@ int main()
 		else
 		{
 			printf("密碼錯誤!!!\n");
-			i++;
+			j++;
 		}
 	}
-	if(i==3)
+	if(j==3)
 	{
 		system("pause");
 		return 0;
 	}
 	getch();
 	system("CLS");
-	//2.清除螢幕，出現主選單 
+	//2.show menu
+	seats_set();//重製座位表 
 	printf("--------------------------\n");
 	printf("| a. Availble seats      |\n");
 	printf("| b. Arrange for you   	 |\n");
-	printf("| c. Choose by youself		 |\n");
+	printf("| c. Choose by youself	 |\n");
 	printf("| d. Exit		 |\n");
 	printf("--------------------------\n");
-	
+	ch=getche();//偵測使用者按鍵並回應
+	printf("\n");
+	if(ch=='A'||ch=='a')
+	{
+		system("CLS");
+		printf("當前座位表\n");
+		show_seats();
+
+	}
+
 	getch();
 	return 0;
 }
