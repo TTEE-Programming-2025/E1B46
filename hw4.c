3e0d946bb40dd22b08a095d138454dad2988879c
Dove411302246 <u11302246@o365.ttu.edu.tw>
2025-05-26 21:28:33 +0800

    finish home work 3

--- hw4.c
+++ hw4.c
@@ -2,7 +2,15 @@
 #include<conio.h>
 #include<stdlib.h>
 #include<string.h>
- 
+
+struct students{
+	char name[50];
+    int id;
+	int math;
+    int physics;
+    int english;
+};
+
 int main()
 {
 	
@@ -66,9 +74,55 @@ int main()
 	printf("-----------------------------------\n");
 	ch=getche();//偵測使用者按鍵並回應
 	printf("\n");
+	int n=0,i=0;
+	struct students students[10];
 	if(ch=='A'||ch=='a')
 	{
+		printf("請輸入學生人數 (5~10):");
+        scanf("%d", &n);
+		while(n<5||n>10)
+		{
+		printf("請輸入學生人數 (5~10):");
+        scanf("%d", &n);
+		}
+            for (i=0;i<n;i++) {
+                printf("\n第%d位學生資料:\n",i+1);
+                printf("姓名:");
+                scanf("%s",&students[i].name);
+                printf("請輸入學號9位數:");
+                scanf("%d", &students[i].id);
+                if (students[i].id<100000000||students[i].id>999999999) {
+                    printf("學號錯誤 請重新輸入\n");
+                    i--; // 回到前一個學生
+                    continue;
+                }
 
+                printf("數學成績:");
+                scanf("%d", &students[i].math);
+                if (students[i].math<0||students[i].math>100) {
+                    printf("成績輸入錯誤 請重新輸入\n");
+                    i--;
+                    continue;
+                }
+
+                printf("物理成績:");
+                scanf("%d", &students[i].physics);
+                if (students[i].physics<0||students[i].physics>100) {
+                    printf("成績輸入錯誤 請重新輸入\n");
+                    i--;
+                    continue;
+                }
+
+                printf("英文成績:");
+                scanf("%d", &students[i].english);
+                if (students[i].english < 0 || students[i].english > 100) {
+                    printf("成績輸入錯誤 請重新輸入\n");
+                    i--;
+                    continue;
+                }
+            }
+			system("CLS");
+			goto menu;
 	}
 	else if(ch=='B'||ch=='b')
 	{
@@ -107,4 +161,3 @@ int main()
 	getch();
 	return 0;
 }
-
